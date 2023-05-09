051d09a82059c5ca52d9a22a55305925e5daafcd
Thomas <thomastex.2013@gmail.com>
2023-05-09 15:06:18 -0300

    optimizo el codigo de .js de /page/predicacion/predicacion. Cargo promgrama de limpieza en tablero. Subo prueba de agenda

--- pages/predicacion/index.js
+++ pages/predicacion/index.js
@@ -1,45 +1,36 @@
+const terriContainer = document.getElementById("terriContainer");
+const territories = {};
+
 fetch("./territorios.json")
     .then(respuesta => respuesta.json())
     .then(territorios => {
-        let terriContainer = document.getElementById("terriContainer")
-    
         terriRender(territorios);
+        territories.data = territorios;
+    });
 
-        function terriRender (terrisArray) {
-            terriContainer.innerHTML = " "
-            for (const terri of terrisArray) {
-                let cardTerri = document.createElement("button")
-                cardTerri.className = "buttonTerri"
-                cardTerri.innerHTML = `
-                <h3 class= "buttonNumber" id=${terri.id}> ${terri.id} </3>
-                `
-                terriContainer.append(cardTerri)
-            }
-            let buttons = document.getElementsByClassName("buttonNumber")
-                for (const button of buttons) {
-                    button.addEventListener("click", showTerri)
-                        
-                }
-                function showTerri (e) {
-                    let result = territorios.find(terri => terri.id == e.target.id)
-                        Swal.fire({
-                            imageUrl: `${result.imgUrl}`,
-                            imageWidth: 400,
-                            imageHeight: 200,
-                            showCloseButton: true,
-                            showConfirmButton: true,
-                            idConfirmButton: "rutaBtn",
-                            confirmButtonText: `<a href="${result.ruta}">¿Como llegar? </a>`,
-                            customClass: "alertStyle"
-                        })
-                }
+    function terriRender(terrisArray) {
+        terriContainer.innerHTML = "";
+        terrisArray.forEach(terri => {
+            const cardTerri = document.createElement("button");
+            cardTerri.className = "buttonTerri";
+            cardTerri.innerHTML = ` <h3 class="buttonNumber" id=${terri.id}> ${terri.id} </h3> `;
+            terriContainer.append(cardTerri);
+        });
+        terriContainer.addEventListener("click", showTerri);
+    }
 
+    function showTerri(e) {
+        if (e.target.classList.contains("buttonNumber")) {
+            const result = territories.data.find(terri => terri.id == e.target.id);
+            Swal.fire({
+                imageUrl: `${result.imgUrl}`,
+                imageWidth: 400,
+                imageHeight: 200,
+                showCloseButton: true,
+                showConfirmButton: true,
+                idConfirmButton: "rutaBtn",
+                confirmButtonText: `<a href="${result.ruta}">¿Como llegar? </a>`,
+                customClass: "alertStyle"
+            });
         }
-
-    })
-
-
-
-
-// CLASE ASIGANADA POR SWEET ALERT AL BOTON DE LA NOTIFICACION DE LOS TERRITORIOS
-    // swal2-confirm swal2-styled
+    }
